3e91d70e6169bac189ffc23ae3dee139f74ea01f
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-08-06 20:37:39 +0300

    4

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,16 @@ const text = document.querySelector('.text-container');
 
 const btn = document.querySelector('.btn');
 
-container.addEventListener('mouseover', () => {container.classList.add('hidden'); });
-container.addEventListener('mouseout', () => {container.classList.remove('hidden'); });
+container.addEventListener('mouseover', () => {
+	container.classList.add('hidden');
+});
+container.addEventListener('mouseout', () => {
+	container.classList.remove('hidden');
+});
 
-btn.addEventListener('click', () => { container.classList.remove('hidden'); });
-btn.addEventListener('click', () => { text.classList.toggle('animat'); });
+btn.addEventListener('click', () => {
+	container.classList.remove('hidden');
+});
+btn.addEventListener('click', () => {
+	text.classList.toggle('animat');
+});

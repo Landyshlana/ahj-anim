868dff857d764147529ceb7ee55026a566ea1ec3
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-08-06 20:55:19 +0300

    5

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,3 @@
-const container = document.querySelector('.container');
-const text = document.querySelector('.text-container');
-
-const btn = document.querySelector('.btn');
-
-container.addEventListener('mouseover', () => {
-	container.classList.add('hidden');
-});
-container.addEventListener('mouseout', () => {
-	container.classList.remove('hidden');
-});
-
-btn.addEventListener('click', () => {
-	container.classList.remove('hidden');
-});
-btn.addEventListener('click', () => {
-	text.classList.toggle('animat');
-});
+import Colapse from "./colapse";
+const widget = new Colapse();
+widget.init();

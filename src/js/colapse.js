8d4cd8911bee8169791f9df3c1fc7a8e6ec1ff23
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-08-07 21:14:21 +0300

    7

--- src/js/colapse.js
+++ src/js/colapse.js
@@ -1,22 +1,21 @@
 export default class Colapse {
-    init(){
-        this.container = document.querySelector('.container');
-        this.text = document.querySelector('.text-container');
+  init(){
+    this.container = document.querySelector('.container');
+    this.text = document.querySelector('.text-container');
         
-        this.btn = document.querySelector('.btn');
+    this.btn = document.querySelector('.btn');
+    this.container.addEventListener('mouseover', () => {
+    this.container.classList.add('hidden');
+    });
+    this.container.addEventListener('mouseout', () => {
+    this.container.classList.remove('hidden');
+    });
         
-        this.container.addEventListener('mouseover', () => {
-            this.container.classList.add('hidden');
-        });
-        this.container.addEventListener('mouseout', () => {
-            this.container.classList.remove('hidden');
-        });
-        
-        this.btn.addEventListener('click', () => {
-            this.container.classList.remove('hidden');
-        });
-        this.btn.addEventListener('click', () => {
-            this.text.classList.toggle('animat');
-        });    
-    }
+    this.btn.addEventListener('click', () => {
+    this.container.classList.remove('hidden');
+    });
+    this.btn.addEventListener('click', () => {
+    this.text.classList.toggle('animat');
+    });    
+  }
 }

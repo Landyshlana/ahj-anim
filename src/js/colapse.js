ba2371d02b3f58edf1b7683c2a8ff1fa7376e8a8
Landyshlana <135726642+Landyshlana@users.noreply.github.com>
2024-08-07 20:40:46 +0300

    6

--- src/js/colapse.js
+++ src/js/colapse.js
@@ -1,22 +1,22 @@
-export default class Colapse{
+export default class Colapse {
     init(){
-        const container = document.querySelector('.container');
-        const text = document.querySelector('.text-container');
+        this.container = document.querySelector('.container');
+        this.text = document.querySelector('.text-container');
         
-        const btn = document.querySelector('.btn');
+        this.btn = document.querySelector('.btn');
         
-        container.addEventListener('mouseover', () => {
-            container.classList.add('hidden');
+        this.container.addEventListener('mouseover', () => {
+            this.container.classList.add('hidden');
         });
-        container.addEventListener('mouseout', () => {
-            container.classList.remove('hidden');
+        this.container.addEventListener('mouseout', () => {
+            this.container.classList.remove('hidden');
         });
         
-        btn.addEventListener('click', () => {
-            container.classList.remove('hidden');
+        this.btn.addEventListener('click', () => {
+            this.container.classList.remove('hidden');
         });
-        btn.addEventListener('click', () => {
-            text.classList.toggle('animat');
+        this.btn.addEventListener('click', () => {
+            this.text.classList.toggle('animat');
         });    
     }
 }
